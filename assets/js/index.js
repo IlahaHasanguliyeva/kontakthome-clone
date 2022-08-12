15ce61152ce2c2ec59fc1a9e4098d8203be25903
IlahaH <lecial38@gmail.com>
2022-08-12 11:30:37 +0400

    email button done

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,31 +29,18 @@ searchInput.addEventListener("input", () => {
 // payment modal----------------------------------------------------------------------------
 const payment = document.querySelector("#payment");
 const paymentOverlay = document.querySelector("#payment-overlay");
+const body = document.querySelector("body");
 
 payment.addEventListener("click", () => {
-  const body = document.querySelector("body");
   const modal = document.querySelector(".payment-modal");
   modal.classList.add("show");
-  if (modal.classList.contains("show")) {
-    // Disable scroll
-    body.style.overflow = "hidden";
-  } else {
-    // Enable scroll
-    body.style.overflow = "auto";
-  }
+  body.style.overflow = "hidden";
 });
 
 paymentOverlay.addEventListener("click", () => {
-  const body = document.querySelector("body");
   const modal = document.querySelector(".payment-modal");
   modal.classList.remove("show");
-  if (modal.classList.contains("show")) {
-    // Disable scroll
-    body.style.overflow = "hidden";
-  } else {
-    // Enable scroll
-    body.style.overflow = "auto";
-  }
+  body.style.overflow = "auto";
 });
 
 // section two------------------------------------------------------------------------------
@@ -68,6 +55,11 @@ for (let i = 0; i < btns.length; i++) {
   });
 }
 
+// sale offer cards-----------------------------------------------------------------------
+
+ const offerCardWrapperTop = document.querySelector(".cards-upper")
+ 
+
 // footer ---------------------------------------------------------------------------------
 // scroll to top---
 const scrollTop = document.querySelector(".to-top");
@@ -80,8 +72,11 @@ scrollTop.addEventListener("click", function () {
 });
 
 // email input---
+const mailOverlay = document.querySelector("#mail-overlay");
+const subModal = document.querySelector(".sub-modal");
 const emailInput = document.querySelector(".email");
 const warnIcon = document.querySelector(".fa-triangle-exclamation");
+const tickIcon = document.querySelector(".fa-check");
 const validRegex =
   /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const mailBtn = document.querySelector(".mail-button");
@@ -89,10 +84,26 @@ const mailBtn = document.querySelector(".mail-button");
 emailInput.addEventListener("input", () => {
   if (emailInput.value.match(validRegex)) {
     mailBtn.style.backgroundColor = "#da2c18";
+    mailBtn.style.cursor = "pointer";
     warnIcon.classList.remove("show");
+    tickIcon.classList.add("show");
+    // button click
+    mailBtn.addEventListener("click", () => {
+        emailInput.value = "";
+        tickIcon.classList.remove("show");
+        subModal.classList.add("show");
+        body.style.overflow = "hidden";
+        mailBtn.style.backgroundColor = "gray";
+        mailBtn.style.cursor = "default";
+      }, { once: true });
   } else {
-    warnIcon.classList.remove("show");
     mailBtn.style.backgroundColor = "gray";
     warnIcon.classList.add("show");
+    tickIcon.classList.remove("show");
   }
+});
+
+mailOverlay.addEventListener("click", () => {
+  subModal.classList.remove("show");
+  body.style.overflow = "auto";
 });
